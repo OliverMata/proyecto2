1efdf1b9f810f74a711997f92ef8d81cdf5e5f96
OliverMata <oliverluis05@gmail.com>
2021-02-12 16:14:55 -0400

    Proyecto 2

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace proyecto2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Crea un programa que escriba en pantalla los números del 1 al 10, usando "do..while".
+            int x=0;
+            do
+            {
+                x=x+1;
+                Console.Write("{0} - ", x);
+                Console.ReadKey();
+
+            } while (x<10);
+            
+        }
+    }
+}
